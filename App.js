dc2e91e1a5f3830af9af4f27f2a3cd20bcd8ea03
GriffinMcLennan <mclennangriffin@gmail.com>
2021-09-19 22:36:55 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, KeyboardAvoidingView, SafeAreaView } from 'react-native'
-import HomeScreen from './screens/HomeScreen'
-import CardScreen from './screens/CardScreen'
-import SendScreen from './screens/SendScreen'
-import SettingsScreen from './screens/SettingsScreen'
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCog, faWallet, faCreditCard, faDollarSign, faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
-import TradeButton from './components/TradeButton'
 import TradeModal from './components/TradeModal'
 import MyTabs from './navigation/MyTabs'
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
     const [overlayVisible, setOverlayVisible] = useState(false);
