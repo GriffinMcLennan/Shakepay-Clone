2f21afed68308ca18e09ee7a90ec7ea9c0c35ed8
GriffinMcLennan <mclennangriffin@gmail.com>
2021-09-19 22:19:49 -0700

    Created TradeModal Component
    -Added TradeModal to App.js
    -Cleaned up App.js imports and stylesheet

--- App.js
+++ App.js
@@ -1,32 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, KeyboardAvoidingView, SafeAreaView, TouchableWithoutFeedback } from 'react-native'
 import HomeScreen from './screens/HomeScreen'
 import CardScreen from './screens/CardScreen'
 import SendScreen from './screens/SendScreen'
-import SettingsScreen from './screens/SettingsScreen';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SettingsScreen from './screens/SettingsScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCog, faWallet, faCreditCard, faDollarSign, faExchangeAlt, faHistory, faTags } from '@fortawesome/free-solid-svg-icons'
 import TradeButton from './components/TradeButton'
-import TradeModalOption from './components/TradeModalOption';
-import { Dimensions } from 'react-native';
-
-//undo
+import TradeModal from './components/TradeModal'
 
 const Tab = createBottomTabNavigator();
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
 
 export default function App() {
     const [overlayVisible, setOverlayVisible] = useState(false);
-    const navigation = useNavigation();
 
     const toggleOverlay = () => {
         setOverlayVisible(!overlayVisible);
     }
-    // console.log(overlayVisible);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -36,27 +29,7 @@ export default function App() {
             >
 
                 {
-                    // Create TradeModal component
-                    overlayVisible && <TouchableWithoutFeedback onPress={toggleOverlay} >
-                        <View style={styles.dominant}>
-                            <TouchableWithoutFeedback onPress={() => alert("You clicked")}>
-                                <View style={[styles.modal, styles.infoModal]}>
-                                    <TradeModalOption title="Buy & sell" description="Buy and sell at current prices" Icon={faExchangeAlt} />
-                                    <TradeModalOption title="Recurring buys" description="Automatically buy every day, every week, or every month" Icon={faHistory} />
-                                    <TradeModalOption title="Limit orders" description="Place a buy or sell order at a price you set" Icon={faTags} last={true} />
-
-                                </View>
-                            </TouchableWithoutFeedback>
-
-
-                            <TouchableWithoutFeedback onPress={toggleOverlay}>
-                                <View style={[styles.modal, styles.closeModal]}>
-                                    <Text>Cancel</Text>
-                                </View>
-                            </TouchableWithoutFeedback>
-
-                        </View>
-                    </TouchableWithoutFeedback>
+                    overlayVisible && <TradeModal toggleOverlay={toggleOverlay} />
                 }
 
                 <NavigationContainer>
@@ -117,40 +90,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         width: "100%",
-    },
-    overlay: {
-        // position: "absolute",
-        // bottom: 30,
-        width: windowWidth * 0.9,
-        height: 200,
-        position: "absolute",
-        bottom: 50,
-    },
-    dominant: {
-        position: "absolute",
-        top: -200,
-        zIndex: 1000,
-        width: windowWidth,
-        height: windowHeight + 200,
-        //other potential colour scheme
-        // backgroundColor: 'rgba(22, 43, 74, 0.8)', 
-        backgroundColor: 'rgba(19, 43, 80, 0.7)',
-        alignItems: "center",
-    },
-    modal: {
-        position: "absolute",
-        width: "90%",
-        backgroundColor: "white",
-        borderRadius: 5,
-    },
-    infoModal: {
-        bottom: 165,
-        height: 255,
-    },
-    closeModal: {
-        bottom: 100,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
     },
 });
---
+++ components/TradeModal.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
+import TradeModalOption from './TradeModalOption'
+import { Dimensions } from 'react-native'
+import { faExchangeAlt, faHistory, faTags } from '@fortawesome/free-solid-svg-icons'
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const TradeModal = ({ toggleOverlay }) => {
+    return (
+        <TouchableWithoutFeedback onPress={toggleOverlay} >
+            <View style={styles.dominant}>
+                <TouchableWithoutFeedback onPress={() => alert("You clicked")}>
+                    <View style={[styles.modal, styles.infoModal]}>
+                        <TradeModalOption title="Buy & sell" description="Buy and sell at current prices" Icon={faExchangeAlt} />
+                        <TradeModalOption title="Recurring buys" description="Automatically buy every day, every week, or every month" Icon={faHistory} />
+                        <TradeModalOption title="Limit orders" description="Place a buy or sell order at a price you set" Icon={faTags} last={true} />
+
+                    </View>
+                </TouchableWithoutFeedback>
+
+
+                <TouchableWithoutFeedback onPress={toggleOverlay}>
+                    <View style={[styles.modal, styles.closeModal]}>
+                        <Text>Cancel</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+
+            </View>
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default TradeModal
+
+const styles = StyleSheet.create({
+    dominant: {
+        position: "absolute",
+        top: -200,
+        zIndex: 1000,
+        width: windowWidth,
+        height: windowHeight + 200,
+        //other potential colour scheme
+        // backgroundColor: 'rgba(22, 43, 74, 0.8)', 
+        backgroundColor: 'rgba(19, 43, 80, 0.7)',
+        alignItems: "center",
+    },
+    modal: {
+        position: "absolute",
+        width: "90%",
+        backgroundColor: "white",
+        borderRadius: 5,
+    },
+    infoModal: {
+        bottom: 165,
+        height: 255,
+    },
+    closeModal: {
+        bottom: 100,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
